c325778c20ea00738db729f10aa94248f0767d23
olaaroub <oussamalaarroubi@gmail.com>
2025-11-20 01:51:14 +0100

    pushed assignment-unit1

---
+++ assignment-unit-1.py
@@ -0,0 +1,27 @@
+
+
+print("==>> a- display age multiplied by 2 <<==")
+
+age = 23
+mult = age*2
+print(f"Age multiplied by 2 is: {mult}")
+
+print("==>> b- display location information <<==")
+
+city = "Marrakech"
+country = "Morocco"
+continent = "Africa"
+print(f"Location : {city}, {country}, {continent}")
+
+print("==>> c- Examination schedule <<==")
+
+exam_start = "thursday"
+exam_end = "sunday"
+print(f"The examination starts on {exam_start} and ends on {exam_end}")
+
+print("==>> d- Display temperature <<==")
+
+temperature = 25
+print(f"The current temperature in Marrakech is {temperature} C")
+
+
